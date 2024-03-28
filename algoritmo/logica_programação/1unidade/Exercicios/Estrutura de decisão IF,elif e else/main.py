3fbdc3fcaa7bcb34b9755894f99a028e64c2b007
LeoSergio <leandrooos222@gmail.com>
2024-03-27 21:48:02 -0300

    commit

--- algoritmo/logica_programação/1unidade/Exercicios/Estrutura de decisão IF,elif e else/main.py
+++ algoritmo/logica_programação/1unidade/Exercicios/Estrutura de decisão IF,elif e else/main.py
@@ -1,17 +1,56 @@
 '''
-Exibindo valores em ordem crescente
-Escreva um programa em Python que leia três
-valores inteiros e escreva-os em ordem
-crescente.
+Escreva um programa em Python que leia dia
+e mês de uma determinada data e a
+localização (hemisfério) do usuário e
+determine qual a estação do ano
+correspondente à data.
+
+Essas datas são fixas?
+O que ocorre nos anos bissextos?
+    sul
+primavera
+    22set - 21dez
+verão
+    22dez - 21 marc
+outono
+    22marc - 21 jun
+inverno
+    22jun - 21set
 '''
-print('Digite as seguintes informações:')
-num1 = int(input('Número 1:'))
-num2 = int(input('Número2:'))
-num3 = int(input('Número3:'))
-if (num1 <= num2):   
-    if num2 <= num3:
-        print('Ordem crescente: %i < %i < %i'%(num1,num2,num3))
-elif num1 <= num3:
-    print('Ordem crescente: %i < %i < %i '%(num1,num3,num2))
+import datetime
+print("Qual a estação do ano?")
+
+dataAtual = datetime.date.today()
+day = 27
+month = 12
+
+if month >= 9:
+    if month <= 11:
+        print('Verão no Hemisfério Sul')
+    elif month == 12:
+        if day <= 21:
+            print('Verão no Hemisfério Sul')
+        else:
+            print('Primavera no Hemisfério Sul')
+elif month >= 1:
+    if month <= 3:
+        print('Verão no Hemisfério Sul')
+    elif month == 3:
+        if day <= 21:
+            print('Verão no Hemisfério Sul')
+        else:
+            print('Outono no Hemisfério Sul')
+elif month >= 4:
+    if month <= 5:
+        print('Outono no Hemisfério Sul')
+    elif month == 6:
+        if day <= 21:
+            print('Outono no Hemisfério Sul')
+        else:
+            print('Inverno no Hemisfério Sul')
 else:
-    print('Ordem Crescente: %i < %i < %i '%())
+    print('Inverno no Hemisfério Sul')
+
+
+
+
