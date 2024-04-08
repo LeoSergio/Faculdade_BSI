9667e4457383f4f7752f9823e8c32f9c704fbd0f
LeoSergio <leandrooos222@gmail.com>
2024-04-08 18:28:47 -0300

    commit

--- algoritmo/logica_programação/1unidade/Exercicios/Estrutura de decisão IF,elif e else/Ex29-if simples.py
+++ algoritmo/logica_programação/1unidade/Exercicios/Estrutura de decisão IF,elif e else/Ex29-if simples.py
@@ -12,20 +12,18 @@ day = int(input('Qual o dia atual?'))
 month = int(input('Qual o mês atual?'))
 year = int(input('Qual o Ano atual?'))
 
-if( day == 29) and (month != 2) or (day == 28 and month != 2):
-      print('%s / %s / %s Data inválida'%(day,month,year))
-elif (day <= 31 and day >= 1) and (year > 0) and (month >= 1 and month <= 12 and month !=2):
+
+if (day <= 31 and day >= 1) and (year > 0) and (month >= 1 and month <= 12 and month !=2):
         print('%s / %s / %s Data válida'%(day,month,year))
-else:
-       print('Data inválida')
-        
-if(year % 4 == 0) and (year % 100 != 0 or year % 400 == 0): # Aqui que estar o erro.
-        if (day == 29 and month == 2) and (year > 0) and (month >= 1 and month <= 12):
+elif (day <= 28 and month == 2) and (year > 0) and (month >= 1 and month <= 12):
+        print('Data válida e ano não é bissexto')
+elif (day == 29 and month == 2) and (year > 0) and (month >= 1 and month <= 12):
+        if(year % 4 == 0) and (year % 100 != 0 or year % 400 == 0): # Aqui que estar o erro.
                 print('%s / %s / %s Data válida para o mês de fevereiro'%(day,month,year))
                 print('%s Ano bissexto'%(year))
-elif (day <= 28 and month == 2) and (year > 0) and (month >= 1 and month <= 12):
-     print('Data válida e ano não é bissexto')
+        else:
+                print('%s Data inválida e o ano não é bissexto'%(year))
 else:
-     print('%s o ano não é bissexto'%(year))
+        print('Data inválida')
     
 
