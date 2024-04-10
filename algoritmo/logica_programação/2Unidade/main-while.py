916083ee81936871cd7b1f027be6a7927716789e
LeoSergio <leandrooos222@gmail.com>
2024-04-10 12:29:20 -0300

    commit

--- algoritmo/logica_programação/2Unidade/main-while.py
+++ algoritmo/logica_programação/2Unidade/main-while.py
@@ -9,7 +9,6 @@ cara ou coroa?
 from random import randint
 print('Jogo de cara ou coroa')
 option = input('Deseja Jogar s/n')
-resp =+ 1
 moeda = randint(1,2)
 if option == 's'or option == 'S':
     jog1 =int((input("Escolha ,Cara(1) ou coroa(2)?")))
@@ -26,15 +25,18 @@ if option == 's'or option == 'S':
     while option2 == 's' or option2 == 's':
         jog1 =int((input("Escolha ,Cara(1) ou coroa(2)?")))
         moeda = moeda = randint(1,2)
-    if jog1 == 1 and moeda == 1:
-        print('Você Ganhou, deu Cara')
-    elif jog1 == 1 and moeda == 2:
-        print('Jogador 1 Perdeu, deu coroa')
-    elif jog1 == 2 and moeda == 2:
-        print('Você ganhou, deu coroa.')
-    elif jog1 == 2 and moeda == 1:
-        print('Você perdeu, deu Cara')
-    option2 = input('Jogar novamente s/n?')       
+        if jog1 == 1 and moeda == 1:
+            print('Você Ganhou, deu Cara')
+        elif jog1 == 1 and moeda == 2:
+            print('Jogador 1 Perdeu, deu coroa')
+        elif jog1 == 2 and moeda == 2:
+            print('Você ganhou, deu coroa.')
+        elif jog1 == 2 and moeda == 1:
+            print('Você perdeu, deu Cara')
+        option2 = input('Jogar novamente s/n?')
+    else:
+        print('Fim do programa')
+        
 else:
         print('Fim do programa')
 
