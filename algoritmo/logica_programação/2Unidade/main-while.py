e50234dc48077a997322d0c95a216ed7d13e151e
LeoSergio <leandrooos222@gmail.com>
2024-04-10 11:38:18 -0300

    commit

--- algoritmo/logica_programação/2Unidade/main-while.py
+++ algoritmo/logica_programação/2Unidade/main-while.py
@@ -4,17 +4,38 @@ i = 1
 while i<=3:
 
     i =+ 1
+cara ou coroa?
 '''
-import random
+from random import randint
+print('Jogo de cara ou coroa')
+option = input('Deseja Jogar s/n')
+resp =+ 1
+moeda = randint(1,2)
+if option == 's'or option == 'S':
+    jog1 =int((input("Escolha ,Cara(1) ou coroa(2)?")))
+    if jog1 == 1 and moeda == 1:
+        print('Você Ganhou, deu Cara')
+    elif jog1 == 1 and moeda == 2:
+        print('Jogador 1 Perdeu, deu coroa')
+    elif jog1 == 2 and moeda == 2:
+        print('Você ganhou, deu coroa.')
+    elif jog1 == 2 and moeda == 1:
+        print('Você perdeu, deu Cara')
+    option2 = input('Jogar novamente s/n?')
 
-jog = random.randint(1,6)
-comp = random.randint(1,6)
-print("Jogador : ", jog)
-print("Computador: ", comp)
-if jog > comp:
- print("Jogador Ganhou!")
-elif comp > jog:
- print("Computador Ganhou!")
+    while option2 == 's' or option2 == 's':
+        jog1 =int((input("Escolha ,Cara(1) ou coroa(2)?")))
+        moeda = moeda = randint(1,2)
+    if jog1 == 1 and moeda == 1:
+        print('Você Ganhou, deu Cara')
+    elif jog1 == 1 and moeda == 2:
+        print('Jogador 1 Perdeu, deu coroa')
+    elif jog1 == 2 and moeda == 2:
+        print('Você ganhou, deu coroa.')
+    elif jog1 == 2 and moeda == 1:
+        print('Você perdeu, deu Cara')
+    option2 = input('Jogar novamente s/n?')       
 else:
- print("Empate!")
-print("Fim do Jogo!")
+        print('Fim do programa')
+
+    
