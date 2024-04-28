723ca0a9f4de3662e0601a1b50e4b9bf0b98553c
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-04-27 21:21:41 -0300

    commit

--- algoritmo/logica_programação/2Unidade/projeto_avaliativo/main.py
+++ algoritmo/logica_programação/2Unidade/projeto_avaliativo/main.py
@@ -6,57 +6,68 @@ import os
 
 print('CONSULTA ONLINE')
 resp = input('Você deseja participar? S/N ')
-option =''
-if resp.upper() == 'S':    
-    if option !=0:
+option = ''
+if resp.upper()== 'S':
+    while option != '0':
+        os.system('clear')
         print('''
     Projeto Nutri-Center
-1-Cadastramento de Clientes:
-2-Planos de Atendimento:
-3-Objetivos do Paciente:
-4-Planos de Dietas:
-5-Finalização da Consulta e Resultados:
-6-Informações pessoais:
-''')
-        option = int(input('Escolha sua opção: '))
+    1-Cadastramento de Clientes:
+    2-Objetivos do Paciente:
+    3-Planos de Dietas:
+    4-Finalização da Consulta e Resultados:
+    5-Informações pessoais:
+    0-FINALIZAR PROGRAMA
+    ''')
+        option = input('Escolha sua opção?')
+        if option == '1':
+            print('DIGITE AS INFORMAÕES PEDIDAS.')
+            ######################################################
+            nome = input('Nome Completo: ')
+            idade = int(input('Qual a sua Idade:'))
+            genero = input('Qual o seu Gênero: M/F ')
+            peso = float(input('Qual o seu peso atual: '))
+            altura = float(input('Qual a sua altura atual: '))
+            pacientes=[nome,idade,genero, peso,altura]
 
+            # Imprimindo os dados dos pacientes em formato de coluna
+            print('''
+                Nome: {},
+                Idade: {},
+                Gênero: {},
+                Peso: {},
+                Altura: {}
+
+                ######################
+            ######### CADASTRADO COM SUCESSO ##########
+                ######################
+            '''.format(*pacientes))
+            input('Tecle <ENTER> para continuar...')
+
+
+        #ALTERAÇÕES NECESSÁRIAS. ATENÇÃO!!!
+        #Falta fazer o paciente voltar ao menu de seleção
+        #Tentar bloquear o usuario para ele não fazer mais de um cadastro.
+        #Colocar as informações em apenas um formato. ex: maiuscula(string) para ficar igual.
+        #Corrigir em vez do if colocar while, pode facilitar a movimentação do usuario.
+
+
+        elif option == '2':
+            print('Módulo2')
+            input('Tecle <ENTER> para continuar...')
+        elif option == '3':
+            print('Módulo3')
+        elif option == '4':
+            print('Módulo4')
+        elif option == '5':
+            print('Módulo5')
 else:
-    print('FIM DO PROGRAMA')
+        print('FIM DO PROGRAMA')
+
+
         #MODULO1-CADASTRAMENTO DE PACIENTES
-if option == 1:
-    print('DIGITE AS INFORMAÕES PEDIDAS.')
-######################################################
-    nome = input('Nome Completo: ')
-    idade = int(input('Qual a sua Idade:'))
-    genero = input('Qual o seu Gênero: M/F ')
-    peso = float(input('Qual o seu peso atual: '))
-    altura = float(input('Qual a sua altura atual: '))
-    pacientes=[nome,idade,genero, peso,altura]
-
-    # Imprimindo os dados dos pacientes em formato de coluna
-    print('''
-          Nome: {},
-          Idade: {},
-          Gênero: {},
-          Peso: {},
-          Altura: {}
-
-          ######################
-######### CADASTRADO COM SUCESSO ##########
-          ######################
-'''.format(*pacientes))
 
 
-elif option == 2:
-    print('Módulo2')
-elif option == 3:
-    print('Módulo3')
-elif option == 4:
-    print('Módulo4')
-elif option == 5:
-    print('Módulo5')
-elif option == 6:
-    print('Módulo6')
 
 
 
