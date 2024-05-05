475b3a2acb57ea8732c854f7b3e43aab84102e54
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-05-04 23:30:05 -0300

    commit

--- algoritmo/logica_programação/2Unidade/projeto_avaliativo/main.py
+++ algoritmo/logica_programação/2Unidade/projeto_avaliativo/main.py
@@ -1,71 +1,115 @@
 import os
 
      ##########################################
-##### Projeto Planejamento de Dietas - Versão 1 #####
+##### Projeto Planejamento de Dietas      #####
      #########################################
 
 # Oq foi feito na ultima att do projeto?
 '''
-1-Adicionei funções ao projeto.
-2- se a resposta do usuario for sim, chama a função main e executa ela de acordo com as opções do usuario.
-
-    PROXIMOS PASSOS:
-1-IMPORTANTE. Parar para ajeitar como vai ser a estrutura do programa na execução.
-ex: MÓDULO PACIENTE: 
-    add paciente, remover paciente, verificar informações do paciente.
-FAZER ISSO PARA OS DEMAIS E ARMAZENAR AS IFORMAÇÕES. OBS: Olhar o model do prof.
-
-'''
-
+Conseguir armazenar os dados no dicionário info
+MODULO 1:
+    CADASTRAMENTO DE PACIENTE
+        *FALTA VERIFICAR CPF
+    COLOCAR OPTIONS DE VERIFICAR INFORMAÇÕES DO PACIENTE E DÁ OPÇÃO DE ALTERA-LO.
+    
+'''     
 print('CONSULTA ONLINE')
+info=[]
 def main_menu():
             print('''
         PROJETO NUTRI-CENTER
-      ##########################
-    #[1]- Módulo Paciente:      #
-    #[2]- Módulo Objetivos:     #
-    #[3]- Módulo Dietas:        #
-    #[4]- Módulo Finalização:   #
-    #[5]- Módulo Informações:   #
-    #[0]- SAIR                  #
-      ##########################
-        ''')
-        
-        #MODULO1-CADASTRAMENTO DE PACIENTES
-def perfil():
-    #Adicionando paciente    
-    print('DIGITE AS INFORMAÕES PEDIDAS.')
-    nome = input('Nome Completo: ')
-    nome_upper = nome.upper()
-    idade = int(input('Qual a sua Idade:'))
-    genero = input('Qual o seu Gênero: M/F ')
-    peso = float(input('Qual o seu peso atual: '))
-    altura = float(input('Qual a sua altura atual: '))
-    info = [nome_upper,idade,genero,peso,altura]
-        
-    # Imprimindo os dados dos pacientes em formato de coluna
-    print('''
-        
-    | Nome: {}                                   
-    | Idade: {} anos                             
-    | Gênero: {}                                 
-    | Peso: {} KG                                
-    | Altura: {} Metros                          
 
-        
-              ######################
-    ######### CADASTRADO COM SUCESSO ##########
-              ######################
-    '''.format(*info))
-    input('Tecle <ENTER> para continuar...') #colocar nas outras function
+    ###########################
+
+    [1]- Módulo Paciente Objetivos: 
+    [2]- Módulo Dietas:        
+    [3]- Módulo consulta:   
+    [4]- Módulo Informações:   
+    [0]- SAIR  
+
+    ###########################
+        ''')
+
+        #MODULO1-CADASTRAMENTO DE PACIENTES
+def cad_paciente():
+    print('''
+        PROJETO NUTRI-CENTER
+
+    ###########################
+
+    [1]- Cadastrar-se: 
+    [2]- Verificar informações:        
+    [3]- Alterar informações:   
+    [4]- Remover Usuário:   
+    [0]- SAIR  
+
+    ###########################
+        ''')
+    option = input('Qual sua opção? ')
+    while option!='0':
+        if option == '1':
+            cad = input('Cadastra-se S/N ')
+            if cad.upper()=='N':
+                print('Escolha outra opção!')
+                main_menu()  
+            while cad.upper() == 'S':
+                #Adicionando paciente    
+                print('DIGITE AS INFORMAÕES PEDIDAS.')
+                nome = input('Nome Completo: ')
+                nome_upper = nome.upper()
+                cpf = input('Digite seu CPF: ')
+                idade = input('DATA DE NASCIMENTO: dd/mm/aaaa ') #DATA DENASCIMENTO
+                genero = input('Qual o seu Gênero: M/F ')
+                peso = float(input('Qual o seu peso atual: '))
+                altura = float(input('Qual a sua altura atual: '))
+                imc = peso/(altura**2)
+                
+                cadastro = { # dicionário
+                    "Nome": nome_upper,
+                    "CPF": cpf,
+                    "Idade": idade,
+                    "Gênero": genero,
+                    "Peso": peso,
+                    "Altura": altura,
+                    "IMC": imc
+                }
+                info.append(cadastro)
+                
+                # Imprimindo os dados dos pacientes em formato de coluna
+                print('''
+
+                | Nome: {}                                   
+                | Idade: {} anos                             
+                | Gênero: {}                                 
+                | Peso: {} KG                                
+                | Altura: {} Metros                          
+
+
+                            ######################
+                ######### CADASTRADO COM SUCESSO ##########
+                            ######################
+                '''.format(*cadastro))
+                input('Tecle <ENTER> para continuar...') #colocar nas outras function
+                cad = input('Adicionar outro cadastro? S/N ')
+        elif option == '2':
+            print('verificar_info()')
+        elif option == '3':
+            print('alterar_info()')
+        elif option == '4':
+            print('remover_usuario()')
+        elif option == '0':
+            print('Fim do programa')
+        else:
+            print('OPÇÃO INVÁLIDA!')
+            option = input('Digite outra opção: ')
 def main():
     resp = input('Você deseja participar? S/N ')
     if resp.upper() == 'S':
         main_menu()
-        option = input('Escolha uma opção?')
+        option = input('Escolha uma opção? --> ')
         while option != '0':
             if option == '1':
-                perfil()
+                cad_paciente()
                 main_menu()
                 option = input('Digite outra opção: ')
             elif option == '2':
@@ -90,19 +134,19 @@ def main():
                 option = input('Digite outra opção: ')
             elif option == '0':
                 print('Programa finalizado.')
-               
+                
             else:
                 print('OPÇÃO INVÁLIDA')
                 option = input('Digite outra opção: ')
         print('FIM DO PROGRAMA')        
     else:
         print('FIM DO PROGRAMA')
-            
-            
+
+print(info)
 if __name__ == "__main__":
     main() #para inicializar a function main
 
-       
+
 
 
 
