e3a437bf9fa45eb3c40c983b83ac3c67ed66781d
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-12-03 13:35:51 -0300

    commit

--- 2periodo/main.c
+++ 2periodo/main.c
@@ -59,3 +59,34 @@ int main() {
     return 0;
 }
 
+
+
+
+
+
+int tela_main(void){    
+    int op;
+    printf("\n");
+    printf("===========================================================\n");
+    printf("=====                SIG-Building                     =====\n");
+    printf("===========================================================\n");
+    printf("=====    Sistema de gestão de material de construção  =====\n");
+    printf("===========================================================\n");
+    printf("=====                [1] - Clientes                   =====\n");
+    printf("=====                [2] - Vendas                     =====\n");
+    printf("=====                [3] - Funcionário                =====\n");
+    printf("=====                [4] - Produto                    =====\n");
+    printf("=====                [5] - Relatório                  =====\n");
+    printf("=====                [6] - Sobre o Projeto            =====\n");
+    printf("=====                [0] - Sair                       =====\n");
+    printf("===========================================================\n");
+    printf("Sua Escolha: --> ");
+
+    if (scanf("%d", &op) != 1) {  // Valida se a entrada foi um número
+        while (getchar() != '\n');  // Limpa o buffer de entrada
+        return -1;  // Retorna um valor fora do intervalo esperado para indicar erro
+    }
+
+    return op;
+}
+
