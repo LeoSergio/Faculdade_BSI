4763e2ff4e9272ad4c42aa78fe377082f181ac16
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2024-12-02 21:44:00 -0300

    commit

--- 2periodo/main.c
+++ 2periodo/main.c
@@ -18,3 +18,44 @@ int main(void) {
  return 0;
 }
 
+
+int main() {
+        int op;
+
+    printf("Iniciando o programa...\n");
+
+    do {
+        op = tela_main(); // Supomos que tela_main exibe o menu e retorna uma opção válida ou inválida
+        if (op >= 0 && op <= 6) {  // Verifica se a opção está no intervalo esperado
+            switch (op) {
+                case 1:
+                    tela_mod_cliente();
+                    break;
+                case 2:
+                    tela_mod_vendas();
+                    break;
+                case 3:
+                    tela_mod_func();
+                    break;
+                case 4:
+                    tela_mod_prod();
+                    break;
+                case 5:
+                    tela_mod_relat();
+                    break;
+                case 6:
+                    tela_info_proj();
+                    break;
+                case 0:
+                    printf("Saindo...\n");
+                    break;
+            }
+        } else {
+            printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (op != 0);
+
+    printf("FIM DO PROGRAMA!\n");
+    return 0;
+}
+
