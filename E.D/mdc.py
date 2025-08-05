a475cc9d845e0358c837833b322d3e498d1d12b1
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-08-05 16:52:59 -0300

    test

---
+++ E.D/mdc.py
@@ -0,0 +1,12 @@
+
+'''
+print('Welcome')
+n = input(int('Digite um numero: -> '))
+m = input(int('Digite outro numero: '))
+respn = n%0 and m%0
+
+for x in range(n):
+    if respn%0:
+        print(respn)
+
+'''
